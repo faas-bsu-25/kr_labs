c53bf44564369a0afb7da4730d92c0d173a8946b
Kat R. <153114658+KxtR-27@users.noreply.github.com>
2025-09-22 11:14:33 -0400

    Restructure assets folder, add coin pickup sound

--- lab6_platformer/scripts/coin.gd
+++ lab6_platformer/scripts/coin.gd
@@ -2,6 +2,6 @@ extends Area2D
 
 func _on_body_entered(body: Node2D) -> void:
 	if body.name == "Player":
-		# Coins           TestLevel
 		self.get_parent().get_parent().coin_collected()
+		body.get_node("CoinPickupSound").play()
 		self.queue_free()
